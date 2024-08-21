2d2952a691a79cfb4669fb125673695e6bcbd626
TanmaySubhedar <tanmaysubhedar03@gmail.com>
2024-08-22 01:42:29 +0530

    Initial setup done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Display from "../components/Display.svelte";
+  import Footer from "../components/Footer.svelte";
+  import Nav from "../components/Nav.svelte";
+  import Product from "../components/Product.svelte";
+
+</script>
+
+<main>
+    <Nav/>
+    <Display/>
+    <Product/>
+    <Footer/>
+
+</main>
