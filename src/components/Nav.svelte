2f1619de4f487c88f1d8e3cf3b19828db529fcef
TanmaySubhedar <tanmaysubhedar03@gmail.com>
2024-08-22 02:08:25 +0530

    done with nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<nav class="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <div>
+        <img  class="h-20 w-20 rounded-full object-cover" src="https://cdn.soloist.ai/264cae70-434f-41fd-9d2a-a030802c1810/logo/2bfd6c37-3827-45d3-b6cf-e5f70014546a.jpg" alt="Logo">
+    </div>
+    <div class="flex  space-x-8 items-center">
+        <a class="hover:text-green-500" href="#home">Home</a>
+        <a class="hover:text-green-500" href="#products">Products</a>
+        <a class="hover:text-green-500" href="#VTON">Virtual TryOn</a>
+        <a class="hover:text-green-500" href="#contact">Contact Us</a>
+        <a class="hover:text-green-500" href="#about">About Us</a>
+        <button class="bg-green-500 hover:bg-green-600 focus:bg-green-700 text-white py-2 px-4 rounded transition-colors">Logout</button>
+    </div>
+
+</nav>
